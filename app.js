f363ca126efa6f93a96f8e69cb1c95c37a665063
CpmJhon21 <cpmjho@gmail.com>
2024-12-21 12:16:42 +0700

    app.js

--- app.js
+++ app.js
@@ -1,21 +1,81 @@
-const express = require("express");
-const ytdl = require("ytdl-core");
-const app = express();
 
-app.get("/download", async (req, res) => {
-  const videoURL = req.query.url;
+import axios from 'axios';
+import CryptoJS from 'crypto-js';
 
-  if (!ytdl.validateURL(videoURL)) {
-    return res.status(400).send("Invalid YouTube URL");
+const spotify = {
+  dl: async function(link) {
+    return this.submit(link);
+  },
+
+  generateToken: function(e, t = "neoxr") {
+    const vn = CryptoJS;
+    const Bg = 256;
+    const Dg = 128;
+    const Sg = 100;
+    
+    const r = vn.lib.WordArray.random(16);
+    const n = vn.PBKDF2(t, r, { keySize: Bg / 32, iterations: Sg });
+    const o = vn.lib.WordArray.random(Dg / 8);
+    const a = vn.AES.encrypt(e, n, { iv: o, padding: vn.pad.Pkcs7, mode: vn.mode.CBC });
+    
+    return r.toString() + o.toString() + a.toString();
+  },
+
+  generateJWT: function(payload, secret) {
+    const header = {
+      alg: "HS256",
+      typ: "JWT"
+    };
+    
+    const enc = (segment) => 
+      CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(JSON.stringify(segment)))
+        .replace(/=+$/, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_');
+    
+    const encx = enc(header);
+    const encxx = enc(payload);
+    
+    const signature = CryptoJS.HmacSHA256(encx + "." + encxx, secret);
+    const encSign = CryptoJS.enc.Base64.stringify(signature)
+      .replace(/=+$/, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+    
+    return `${encx}.${encxx}.${encSign}`;
+  },
+
+  submit: async function(link) {
+    const jwtPayload = {
+      create: Date.now(),
+      iat: Math.floor(Date.now() / 1000),
+      exp: Math.floor(Date.now() / 1000) + 300 
+    };
+    
+    const jwt = this.generateJWT(jwtPayload, 'daffa875');
+
+    const token = this.generateToken(link);
+  
+    try {
+      const response = await axios.post("https://api.ssspotify.buzz/v1/ajax", JSON.stringify({ q: link }), {
+        headers: {
+          'authority': 'api.ssspotify.buzz',
+          'accept': 'application/json, text/plain, */*',
+          'authorization': `Bearer ${jwt}`,
+          'content-type': 'application/json',
+          'origin': 'https://ssspotify.buzz',
+          'referer': 'https://ssspotify.buzz/',
+          'user-agent': 'Postify/1.0.0',
+          'x-api-token': token
+        }
+      });
+  
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
+};
 
-  const info = await ytdl.getInfo(videoURL);
-  const title = info.videoDetails.title;
-
-  res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
-  ytdl(videoURL, { filter: "audioonly", quality: "highestaudio" }).pipe(res);
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+export { spotify };
