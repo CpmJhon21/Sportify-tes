d7a37a8c508eba339e28a01e9f33d1d7d453d441
CpmJhon21 <cpmjho@gmail.com>
2024-12-21 12:12:26 +0700

    app.js

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-const token = 'BQCIB6hnmeA9LsoH-dT1bLs9j1WUSOFYOyiNiV0Bj5WQOZAL-_VlD3y_dnk4bJ_41inAPpDvblYbrnxLa3aN_QtQo5EgDg6J4zYypff2Xk30K8DhR3xA4q6_8wVnx935ZS3X35LdXcGfZHD9rzks7xoIhKLA_wsBWSQVrRIyZLKOeCOyZ4m-wxsLJIQsbyflbZLKFCayJF46xFVvUHCqGrlgQtnKnwkb_CLipsETXOzvKECKt1By8t5HvIPEg6-uAhHQZMnYUte3-yRQiWr5zmTfbd7VWovV';
-async function fetchWebApi(endpoint, method, body) {
-  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    method,
-    body:JSON.stringify(body)
-  });
-  return await res.json();
-}
+const express = require("express");
+const ytdl = require("ytdl-core");
+const app = express();
 
-async function getTopTracks(){
-  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
-  return (await fetchWebApi(
-    'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
-  )).items;
-}
+app.get("/download", async (req, res) => {
+  const videoURL = req.query.url;
 
-const topTracks = await getTopTracks();
-console.log(
-  topTracks?.map(
-    ({name, artists}) =>
-      `${name} by ${artists.map(artist => artist.name).join(', ')}`
-  )
-);
+  if (!ytdl.validateURL(videoURL)) {
+    return res.status(400).send("Invalid YouTube URL");
+  }
+
+  const info = await ytdl.getInfo(videoURL);
+  const title = info.videoDetails.title;
+
+  res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
+  ytdl(videoURL, { filter: "audioonly", quality: "highestaudio" }).pipe(res);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
